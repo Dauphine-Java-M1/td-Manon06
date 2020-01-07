2bef64e4cfcf3f671c49091b4bbbdfaa8ce357be
Manon06 <manon.tourbier@dauphine.eu>
2020-01-07 16:15:03 +0100

    ajout de réponses MySafeList

--- Threads/src/MySafeList.java
+++ Threads/src/MySafeList.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Objects;
 
-public class MySafeList {
+public class MySafeList extends ArrayList<Double> {
+
+    ArrayList<Double> list;
+    //private Object monitor = new Object();
+   // int count =0;
+
+
+
+    public MySafeList() {
+        list = new ArrayList<Double>();
+    }
+
+    public void add(Double e){
+        list.add(e);
+    }
+
+    public int size(){
+        return list.size();
+    }
+
+    public Double get(int i){
+        return list.get(i);
+    }
+
+
+
 }

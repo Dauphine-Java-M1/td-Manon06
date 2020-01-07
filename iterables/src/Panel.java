e6bcf9e00590d9b675a035aeeb811b80746f8d15
Manon06 <manon.tourbier@dauphine.eu>
2020-01-07 13:29:05 +0100

    Modifications exercice 1

--- iterables/src/Panel.java
+++ iterables/src/Panel.java
@@ -1,36 +1,64 @@
 import java.util.AbstractList;
 import java.util.Iterator;
 
-public class Panel {
+public class Panel extends AbstractList<Integer> {  // intervalle de nombres entiers
 
     private int upperBound;
     private int lowerBound;
 
 
-    // Question 1 Exercice 1
+    public Panel(final int lb, final int ub) {     // Construction du panel
+        this.lowerBound = lb;
+        this.upperBound = ub;
+    }
 
-    public static Iterator<Integer>panel1(final int i,final int j) {
-
+    public static Iterator<Integer> panel(final int lb, final int ub) {
         return new Iterator<Integer>() {
 
-            private int current=i;
+            private int curr = lb;
 
+            @Override
             public boolean hasNext() {
-                return current<=j;
+                return (curr <= ub);
             }
 
+            @Override
             public Integer next() {
-                return current++;
-            }
-        };
-    }
+
+                if (this.hasNext()){
+                    return (curr++);
+                }
+            return null;
+        }
+    };
+
+}
+
 
     public static void main(String[] args){
-    Iterator<Integer> it=panel1(1,5);
-    for(;it.hasNext();)
-    System.out.println(it.next());
+
+            // Question 1
+
+        System.out.println("ça marche 1");
+        Iterator<Integer> it=panel(1,5);
+        for(;it.hasNext(); System.out.println(it.next())); // affiche 1 2 3 4 5
+        System.out.println("ça marche 2");
+
+
     }
 
+
+
+
+    @Override
+    public Integer get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 
 
 

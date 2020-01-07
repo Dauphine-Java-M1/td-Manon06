2edb280511152ab5e65fb4908634e122a63b21c5
Manon06 <manon.tourbier@dauphine.eu>
2020-01-07 15:40:24 +0100

    Fin Threadtest

--- Threads/src/ThreadTest.java
+++ Threads/src/ThreadTest.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+
+
 
 public class ThreadTest {
+
+
+    private static int i=0;
+    private static final Object monitor = new Object();
+
+    public static Runnable method(final int id) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                while (i < 10000) {
+                    synchronized (monitor) {
+                        i++;
+                        if (i <= 10000) {
+                            System.out.print(" Je suis le thread numéro " + id + " de l'itération " + i);
+                            System.out.print("\n");
+                        }
+                    }
+                }
+            }
+        };
+    }
+
+
+
+    public static void main(String[] args) {
+        Thread t1 =  new Thread(method(1));
+        Thread t2 =  new Thread(method(2));
+
+        t1.start();
+        t2.start();
+    }
+
+
 }
+
+
